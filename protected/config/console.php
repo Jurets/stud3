82c9e3141270689dd3850ddd56d16e7dd85d6be4
Jurets <jurets75@gmail.com>
2013-12-28 11:44:49 +0200

    некоторые изменения (настройки, тз)

--- protected/config/console.php
+++ protected/config/console.php
@@ -11,14 +11,18 @@ return array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 		),
 		// uncomment the following to use a MySQL database
-		/*
+		
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
-			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => '',
-			'charset' => 'utf8',
+            'class'=>'CDbConnection',
+            'connectionString' => 'mysql:host=localhost;dbname=freestud3',
+            //'emulatePrepare' => true,
+            'username' => 'freestud3',
+            'password' => 'freestud3q1',
+            'charset' => 'utf8',
+            'tablePrefix' => 'ci_',
+            //'enableProfiling'=>true,
+            'schemaCachingDuration' => 1000
 		),
-		*/
+		
 	),
 );
