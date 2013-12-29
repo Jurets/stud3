c77dabe3543d72280f6ba4052e3d976596a6fde0
Jurets <jurets75@gmail.com>
2013-12-30 00:23:52 +0200

    убрать пробу

--- index.php
+++ index.php
@@ -8,9 +8,9 @@ $config=dirname(__FILE__).'/protected/config/main.php';
 date_default_timezone_set("Europe/Moscow");
 
 // remove the following lines when in production mode
-// defined('YII_DEBUG') or define('YII_DEBUG',true);
+ defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
-// defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+ defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
 require_once($yii);
 Yii::createWebApplication($config)->run();
